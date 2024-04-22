6554d846f185cce849c3d3880e5fb35c9d79bd22
Halid Erdur <haliderdur@gmail.com>
2024-04-22 23:53:48 +0200

    Playwright learning initial commit

---
+++ src/test/java/com/halid/tests/day01/playwrightIntro/PlaywrightTests_P01.java
@@ -0,0 +1,32 @@
+package com.halid.tests.day01.playwrightIntro;
+
+import com.microsoft.playwright.*;
+
+import java.util.List;
+
+public class PlaywrightTests_P01 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Playwright playwright = Playwright.create();
+
+        BrowserType chromium = playwright.chromium();
+        Browser browser = chromium.launch(new BrowserType.LaunchOptions().setHeadless(false));
+        BrowserContext contexts = browser.newContext();
+        Page page = browser.newPage();
+        page.navigate("https://www.google.com");
+
+        page.click("id=L2AGLb");
+        page.click("textarea[title='Search']");
+        page.keyboard().insertText("apple");
+        page.keyboard().press("Enter");
+
+        page.waitForTimeout(2000);
+
+
+
+        page.close();
+        browser.close();
+        playwright.close();
+
+    }
+}
