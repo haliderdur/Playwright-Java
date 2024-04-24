2be7eaee3dda614da5aef7ab28cd324841a3bff4
Halid Erdur <haliderdur@gmail.com>
2024-04-24 09:59:14 +0200

    asd

--- src/test/java/com/halid/tests/day01/playwrightIntro/PlaywrightTests_P01.java
+++ src/test/java/com/halid/tests/day01/playwrightIntro/PlaywrightTests_P01.java
@@ -1,7 +1,9 @@
 package com.halid.tests.day01.playwrightIntro;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
 
+import java.nio.file.Paths;
 import java.util.List;
 
 public class PlaywrightTests_P01 {
@@ -9,22 +11,40 @@ public class PlaywrightTests_P01 {
     public static void main(String[] args) throws InterruptedException {
         Playwright playwright = Playwright.create();
 
+        // set Browser object
         BrowserType chromium = playwright.chromium();
-        Browser browser = chromium.launch(new BrowserType.LaunchOptions().setHeadless(false));
-        BrowserContext contexts = browser.newContext();
-        Page page = browser.newPage();
-        page.navigate("https://www.google.com");
+        // start Browser
+        Browser browser = chromium.launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(0));
 
-        page.click("id=L2AGLb");
-        page.click("textarea[title='Search']");
-        page.keyboard().insertText("apple");
-        page.keyboard().press("Enter");
+        /*
+        // start new window
+         BrowserContext window = browser.newContext();
+        */
 
-        page.waitForTimeout(1000);
+        // start new tab
+        Page pageTab = browser.newPage();
 
+        // open google page
+        pageTab.navigate("https://www.google.com");
 
-        page.close();
+        // accept cookies
+        pageTab.click("id=L2AGLb");
+        // click search input box
+        pageTab.click("textarea[title='Search']");
+        // type apple
+        pageTab.keyboard().insertText("apple");
+        // press enter
+        pageTab.keyboard().press("Enter");
+        // wait for new page load
+        pageTab.waitForTimeout(1000);
+
+        pageTab.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("google_AppleSearch_screenshot1.png")));
+
+        // close tab
+        pageTab.close();
+        // close browser
         browser.close();
+        // close Playwright
         playwright.close();
 
     }
