c0700b111c93489a4b5dccdb793a752f64a76f1a
Halid Erdur <haliderdur@gmail.com>
2024-04-29 23:55:59 +0200

    Radio buttons

--- src/test/java/com/halid/tests/day02/pac_01_multipleElements_checkbox_radio/P05_RadioButtons.java
+++ src/test/java/com/halid/tests/day02/pac_01_multipleElements_checkbox_radio/P05_RadioButtons.java
@@ -2,6 +2,7 @@ package com.halid.tests.day02.pac_01_multipleElements_checkbox_radio;
 
 import com.halid.utils.BrowserUtils;
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -19,12 +20,37 @@ public class P05_RadioButtons {
 
         page.navigate("https://practice.cydeo.com/radio_buttons");
 
-        ElementHandle blueRadio = page.querySelector("[id=blue]");
-        ElementHandle redRadio = page.querySelector("[id=red]");
 
+        ElementHandle redRadio = page.querySelector("[id=red]");
         redRadio.click();
-        BrowserUtils.sleepWithThread(1);
+        ElementHandle blueRadio = page.querySelector("[id=blue]");
         blueRadio.click();
+
+
+        /*
+        <div class="radio">
+            <input type="radio" id="hockey" name="sport">
+            <label class="form-check-label" for="hockey">Hockey</label>
+        </div>
+
+        This getByRole() method works cuz in html element, label tag designed to be connected with input tag.
+        see id="hockey" and for="hockey"
+         */
+        Locator hockeyRadioBtn = page.getByRole(AriaRole.RADIO, new Page.GetByRoleOptions().setName("Hockey"));
+        hockeyRadioBtn.click();
+
+         /*
+         <div class="radio">
+            <input type="radio" id="red" name="color">
+            <label class="form-check-label">Red</label>
+        </div>
+
+        This getByRole() method does not work cuz in html element, label tag designed NOT to be connected with input tag.
+        see id="red" but there must be for="red" in label tag as well
+         */
+        // Locator redRadioBtn = page.getByRole(AriaRole.RADIO, new Page.GetByRoleOptions().setName("Red"));
+        // redRadioBtn.click();
+
 
         BrowserUtils.sleepWithPage(page, 2);
 
